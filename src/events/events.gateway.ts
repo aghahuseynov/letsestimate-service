b606c743cdaa564b8f75e5264d97abfa9994bc50
agha-huseynov <agha.huseynov@gamesys.ee>
2023-05-02 21:15:24 +0300

    support all origins

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -12,7 +12,7 @@ const roomEstimations: RoomEstimation[] = [];
 
 @WebSocketGateway({
   cors: {
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: ['GET', 'POST'],
     credentials: true,
   },
